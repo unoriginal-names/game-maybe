e86f6a7edb8e17250022b4eb5623cd2335ef0966
creyol <172530311+unoriginal-names@users.noreply.github.com>
2025-05-10 12:01:56 +0100

    nsrtuyhuvilwrhawthvwartawt

--- game/test.js
+++ game/test.js
@@ -8,10 +8,7 @@ delta.getDeltaTime = function(prevTime) {
     return date.getTime() - prevTime
 }
 
-
 function $(query, type) {
-    let func
-
     switch (type) {
         case 1:
             return document.GetElementById(query)
